acc813510d0b80fb7b6261bf1a7a3f8b3254158c
gdoqhuefrygutv <szhu28@riverdale.edu>
2025-01-28 11:38:37 -0500

    finished assignment, looks good! :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(args[0]+args[7]);
+        System.out.print("Today, I went to the"+" "+args[1]) ;
+        System.out.println(" "+"and ate a(n)"+" "+args[0]+" "+"Then, I"+" "+args[2]+" " +"on my way to school.");
+        System.out.print("At the,"+" "+args[3]+" "+"School of Sciences,I learned about the historical figure,"+" "+args[4]);
+        System.out.println(" "+"They are someone who"+" "+args[5]+" "+"good." + " ");
+        System.out.print( "However, they have"+" "+args[7]+" "+"teeth left.");
+
     }
 }
